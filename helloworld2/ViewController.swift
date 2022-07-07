446d7c768b30d04596400e7bb7f5e9c48c1a6b0a
arseniy_v <vetrov.arseniy@gmail.com>
2022-07-07 16:08:16 -0500

    Initial Commit

---
+++ helloworld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloworld2
+//
+//  Created by arseniy vetrov on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
